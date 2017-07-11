558532e3e95ca7710e49e40a61e786cc1868f5a7
Eva <Eva@DESKTOP-CDRCGMN>
2017-07-11 18:19:26 -0400

    Displayed all powers int he Powers List - Driver class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
  * Name: Wayne Pud
  * Date: July 11, 2017
  * Description: Demo for lesson 9
- * Version: 0.3 - Added a power to the SuperHuman object
+ * Version: 0.4 - Added another power to the SuperHuman object
+ *              - Also displayed all the powers in the Power List
  */
 
 
@@ -20,6 +21,8 @@ namespace COMP123_S2017_Lesson9B
         {
             SuperHuman superHuman = new SuperHuman("SuperDude");
             superHuman.AddPower("Spider Climbing", 50);
+            superHuman.AddPower("Webcasting", 40);
+            superHuman.DisplayPowers();
         }
     }
 }
