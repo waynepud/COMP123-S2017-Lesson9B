8dbdd6fa2e6137869ef64ce8b8d77ba8bf389128
Eva <Eva@DESKTOP-CDRCGMN>
2017-07-11 17:29:17 -0400

    Added a class diagram and human abstract class

---
+++ COMP123-S2017-Lesson9B/Human.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/*
+ * Name: Wayne Pud
+ * Date: July 11, 2017
+ * Description: This is the Abstract Human Class
+ * Version: 0.1 - Created the Abstract Human class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the abstract human class
+    /// </summary>
+    public abstract class Human
+    {
+        //private instance variables (Fields)
+        private string _name;
+
+        //public properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        /// <summary>
+        /// This is the main constructor for the human class
+        /// It takes one parameter - name (string)
+        /// </summary>
+        /// <param name="name"></param>
+        //constructors ----------
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+
+
+        //private methods
+
+        //public methods
+    }
+}
