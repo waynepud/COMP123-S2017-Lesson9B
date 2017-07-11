fcce9f40548a5ce0195a7e16c76162943fc44c82
Eva <Eva@DESKTOP-CDRCGMN>
2017-07-11 17:35:43 -0400

    Added SuperHuman class

---
+++ COMP123-S2017-Lesson9B/SuperHuman.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Wayne Pud
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman class that extends the human class
+ * Version: 0.1 - Created the superhuman class
+ */
+namespace COMP123_S2017_Lesson9B
+{
+    /// <summary>
+    /// This is the SuperHuman class.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private instance variables(field)
+
+
+
+        //Public properties
+
+        //Constructors
+
+        public SuperHuman(string name) : base (name)
+        {
+
+        }
+
+        //Private methods
+
+        //Public methods
+    }
+}
