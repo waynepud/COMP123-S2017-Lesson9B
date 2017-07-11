a0b63e176344f656a49ad64dfa23c9b82b03d356
Eva <Eva@DESKTOP-CDRCGMN>
2017-07-11 17:12:32 -0400

    Created program class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Name: Wayne Pud
+ * Date: July 11, 2017
+ * Description: Demo for lesson 9
+ * Version: 0.1 - Created the program class
+ */
+
+
 namespace COMP123_S2017_Lesson9B
 {
     class Program
