354eb8be85e9cce9cdfb5ad4a6aadc149d662afd
Eva <Eva@DESKTOP-CDRCGMN>
2017-07-13 13:56:59 -0400

    Added constructor to supervillain class

--- COMP123-S2017-Lesson9B/SuperVillian.cs
+++ COMP123-S2017-Lesson9B/SuperVillian.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name: Wayne Pud
  * Date: July 13, 2017
  * Description: This is the SuperHero class which inherits from the SuperHuman class and implements the IHasMalice Interface
- * Version: 0.1 - Created SuperVillain
+ * Version: 0.2 - added constructor to SuperVillain class
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -18,6 +18,23 @@ namespace COMP123_S2017_Lesson9B
         private int _malice;
 
         // PUBLIC PROPERTIES
+        public int Malice
+        {
+            get
+            {
+                return this._malice;
+            }
+            set
+            {
+                this._malice = value;
+            }
+        }
+
+        //CONSTRUCTORS
+        public SuperVillian(string name, int malice) : base (name)
+        {
+            this.Malice = malice;
+        }
 
     }
 }
